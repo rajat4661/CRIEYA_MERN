d71446a66701ad323ee2a6a772175bd2461188c0
utkrao <109218765+ut00@users.noreply.github.com>
2023-01-02 13:41:58 +0530

    resolved active issue

--- client/src/components/lib/NavigationBar.js
+++ client/src/components/lib/NavigationBar.js
@@ -1,49 +1,64 @@
-import React , {Component} from 'react'
+import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
 
-
-import "../../Styles/NavigationBar.css"
+import "../../Styles/NavigationBar.css";
 
 import logo from "../../images/logo.png";
 
 class NavigationBar extends Component {
-  state={clicked:false}
+  state = { clicked: false };
 
-  handleClick = ()=>{
-    this.setState({clicked: !this.state.clicked})
-  }
+  handleClick = () => {
+    this.setState({ clicked: !this.state.clicked });
+  };
 
-render(){
-  return (
-    <>
-      <nav id='Navigation' >
-         <a to='index.html'>
+  render() {
+    return (
+      <>
+        <nav id="Navigation">
+          <a to="index.html">
             <img src={logo} alt="" />
-         </a>
-         <div>
-            <ul id="Navigation-items" className={this.state.clicked ? '#Navigation-items active' : '#Navigation-items'}>
-                <li><NavLink className="active" to='/'> Home</NavLink></li>
-                <li><NavLink to='/about'> About </NavLink></li>
-                <li><NavLink to='/contact'> Contact</NavLink></li>
-                <li><NavLink to='/signup'> Signup</NavLink></li>
-                <li><NavLink to='/login'> Login</NavLink></li>
-
+          </a>
+          <div>
+            <ul
+              id="Navigation-items"
+              className={
+                this.state.clicked
+                  ? "#Navigation-items active"
+                  : "#Navigation-items"
+              }
+            >
+              <li>
+                <NavLink className={this.state.clicked ? "active": ""} to="/">
+                  {" "}
+                  Home
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/about"> About </NavLink>
+              </li>
+              <li>
+                <NavLink to="/contact"> Contact</NavLink>
+              </li>
+              <li>
+                <NavLink to="/signup"> Signup</NavLink>
+              </li>
+              <li>
+                <NavLink to="/login"> Login</NavLink>
+              </li>
             </ul>
-         </div>
+          </div>
 
-
-         <div id='Navigation-mobile' onClick={this.handleClick}>
-            <i id="bar" className={this.state.clicked ?'fas fa-times' : "fas fa-bars"}></i>
-            
-         </div>
-
-
-      </nav>
-    
-    
-    </>
-  )
-}
+          <div id="Navigation-mobile" onClick={this.handleClick}>
+            <i
+              id="bar"
+              className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}
+            ></i>
+          </div>
+        </nav>
+      </>
+    );
+  }
 }
 
 export default NavigationBar;
