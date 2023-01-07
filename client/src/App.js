f9afd70e72a044c36c84f1536ce7d6c8ae44cf6e
utkrao <109218765+ut00@users.noreply.github.com>
2023-01-07 17:35:24 +0530

    fixed problems

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,3 @@
-import "./App.css";
 import { Route } from "react-router-dom";
 
 import { Home } from "./pages/Home";
@@ -8,23 +7,23 @@ import { Contact } from "./pages/Contact";
 import { About } from "./pages/About";
 import { Routes } from "react-router-dom";
 import IdeaForm from "./pages/IdeaForm";
-import  NavigationBar  from "./components/lib/NavigationBar";
-// import { LandingPage } from "./pages/LandingPage";
+import NavigationBar from "./components/lib/NavigationBar";
+import "./App.css";
 
 function App() {
   return (
-    <>
-  <NavigationBar />
-      
+    <div className="App">
+      <NavigationBar />
+
       <Routes>
         <Route exact path="/" element={<Home />}></Route>
         <Route path="/about" element={<About />}></Route>
-        <Route path="/idea-submission-form" element={ <IdeaForm /> }></Route>
+        <Route path="/idea-submission-form" element={<IdeaForm />}></Route>
         <Route path="/login" element={<Login />}></Route>
         <Route path="/signup" element={<Signup />}></Route>
         <Route path="/contact" element={<Contact />}></Route>
       </Routes>
-    </>
+    </div>
   );
 }
 
