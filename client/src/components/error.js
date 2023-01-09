89f6f0fd5f31f46e93f0704617ab9021c52a0127
utkrao <109218765+ut00@users.noreply.github.com>
2023-01-10 00:25:17 +0530

    converted to vite

--- client/src/components/error.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-export const error = () => {
-  return (
-    <div>error 404.....bc error hai</div>
-    
-  )
-}
---
+++ client/src/pages/IdeaForm.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import IdeaSubmissionForm from '../components/IdeaSubmissionForm';
+
+const IdeaForm = () => {
+  return (
+    <IdeaSubmissionForm />
+  )
+}
+
+export default IdeaForm
