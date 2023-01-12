5b78837c0cf52e40569ef0c1e266e6adcec3751e
rajat <rajat.c.4661@gmail.com>
2023-01-12 20:13:59 +0530

    counter done

--- client/src/components/lib/Counter/Counter.jsx
+++ client/src/components/lib/Counter/Counter.jsx
@@ -1,5 +1,7 @@
-import React from 'react'
+import React , {useState} from 'react'
 import "./Counter.css"
+import CountUp from 'react-countup'
+import VisibilitySensor from 'react-visibility-sensor'
 import box4 from "../../../assets/images/box-4.png"
 import box3 from "../../../assets/images/box-3.png"
 import box2 from "../../../assets/images/box-2.png"
@@ -7,6 +9,7 @@ import box1 from "../../../assets/images/box-1.png"
 
 
 const Counter = () => {
+  
   return (
     <>
      <div id="counter">
@@ -14,14 +17,29 @@ const Counter = () => {
 
          <div className="counter-box box-1">
             <img src={box1}alt="" />
-            <h1>74<sup>+</sup></h1>
-            <h3>Technology
-Commercialization</h3>
+            <h1 data-number='74'>
+               <CountUp
+                start={0}
+                end={74}
+                duration={1.2}
+               
+               />
+                   <sup>+</sup>
+                            
+               
+            </h1>
+            <h3>Technology Commercialization</h3>
 
          </div>
          <div className="counter-box box-2">
             <img src={box2}alt="" />
-            <h1>25<sup>+</sup></h1>
+            <h1>
+            <CountUp
+                start={0}
+                end={25}
+                duration={1.2}
+               
+               /><sup>+</sup></h1>
             <h3>Projects  Incubated</h3>
          </div>
 
@@ -34,13 +52,25 @@ Commercialization</h3>
            <div className="counter-box box-3">
            <img src={box3}alt="" />  
            <h3>Projects Funded</h3>
-           <h1>1Cr<sup>+</sup></h1>
+           <h1> <CountUp
+                start={0}
+                end={1}
+                duration={1.2}
+               
+               />
+             Cr<sup>+</sup></h1>
           
            </div>
            <div className="counter-box box-4">
             <img src={box4}alt="" />
             <h3>Mentors</h3>
-           <h1>15<sup>+</sup></h1>
+           <h1>
+           <CountUp
+                start={0}
+                end={15}
+                duration={1.2}
+               
+               /><sup>+</sup></h1>
            </div>
 
       </div>
