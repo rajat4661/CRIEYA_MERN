3af666a9e17a01da4152115ba5a85b17b7be2b93
utkrao <109218765+ut00@users.noreply.github.com>
2023-01-12 20:13:38 +0530

    changed folder structure

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { NavLink, Route, Routes } from "react-router-dom";
 import HeroSection from "../components/Home/HeroSection/HeroSection"
 import Button from "../components/lib/Button";
-import Counter from '../components/lib/Counter/Counter';
+import Counter from "../components/Home/Counter/Counter"
 import IdeaForm from "./IdeaForm";
 
 const Home = () => {
@@ -13,7 +13,7 @@ const Home = () => {
       </Routes>
       <div>
         <HeroSection />
-        <Counter/>
+        <Counter />
         {/* <p className="pt-5">WELCOME</p>
         <h1>We Are The MERN Developer</h1>
         <NavLink to="/idea-submission-form">
